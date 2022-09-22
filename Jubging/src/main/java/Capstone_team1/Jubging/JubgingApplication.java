02f4a8f34f472173cd23834b5ec3e00980cb8e12
99kenny <helloiamkenny@gmail.com>
2022-09-22 16:27:34 +0900

    초기설정

---
+++ Jubging/src/main/java/Capstone_team1/Jubging/JubgingApplication.java
@@ -0,0 +1,13 @@
+package Capstone_team1.Jubging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubgingApplication.class, args);
+	}
+
+}
---
+++ Jubging/src/test/java/Capstone_team1/Jubging/JubgingApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone_team1.Jubging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubgingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
