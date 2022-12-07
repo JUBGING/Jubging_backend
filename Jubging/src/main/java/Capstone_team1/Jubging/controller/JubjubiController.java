6ffcb196aec505c2d8484a39e1ff3501e30d2a8b
99kenny <helloiamkenny@gmail.com>
2022-12-07 22:56:29 +0900

    fix:multipart patch error

--- Jubging/src/main/java/Capstone_team1/Jubging/controller/JubjubiController.java
+++ Jubging/src/main/java/Capstone_team1/Jubging/controller/JubjubiController.java
@@ -48,7 +48,7 @@ public class JubjubiController {
         return jubJubiService.endJubging(endJubgingRequestDto);
     }
 
-    @PatchMapping("/jubjubi/jubging-data/img")
+    @PostMapping("/jubging-data/img")
     @ResponseStatus(value = HttpStatus.OK)
     public SendImageResponseDto sendImage(@RequestBody MultipartFile image){
         return jubJubiService.sendImage(image);
